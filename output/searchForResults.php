c5fad2db3f58475afa1380679e6397898e455484
Stacy <yolanda7zhangwoo@gmail.com>
2020-07-18 10:12:21 -0400

    switch back

--- output/searchForResults.php
+++ output/searchForResults.php
@@ -1,14 +1,9 @@
-<?php
- 
-    $state=$_GET['state'];
-    $date=$_GET['date'];
-    
-    if(empty($state)||empty($date)){
-        echo "<h4>Please enter State and Date</h4>\n";
-        die("Please enter again");
-        
-    }
-    
-    echo "<table><thead><tr><th>State</th> </tr> <tr><th>date</th> </tr></thead><tbody><tr><td>$state</td><td>$date</td></tr></tbody></table>";
-    
-?>
+
+<html>
+<body>
+
+State <?php echo $_POST['state']; ?><br>
+Date <?php echo $_POST['date']; ?><br>
+
+</body>
+</html>
